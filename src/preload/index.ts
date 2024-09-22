b6eae54a1d6df6a6021a08ff58d70fa7dc43b0a7
ellectronchik <electronchik04@mail.ru>
2024-09-22 13:35:17 +0300

    added readClipboard handler to api

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,8 +1,11 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
+import { Api } from '../main/models/Api'
 
 // Custom APIs for renderer
-const api = {}
+const api: Api = {
+  readClipboard: () => ipcRenderer.invoke('clipboard-read')
+}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
