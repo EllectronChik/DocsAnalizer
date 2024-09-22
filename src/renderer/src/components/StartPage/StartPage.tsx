e3fe02fe4f3d0afcb2b885bdb1c49a71f4bca52a
ellectronchik <electronchik04@mail.ru>
2024-09-22 13:40:46 +0300

    Refactor StartPage component with clipboard integration
    - Added context hook to fetch clipboard data
    - Integrated `FileUriContext` for managing `fileUri` state
    - Implemented `fetchClipboard` function with async clipboard reading
    - Added `useEffect` to handle paste events for clipboard input
    - Updated UI with error handling and clipboard paste buttons

--- src/renderer/src/components/StartPage/StartPage.tsx
+++ src/renderer/src/components/StartPage/StartPage.tsx
@@ -1,12 +1,36 @@
-import { FC } from 'react'
+import { FC, useContext, useEffect } from 'react'
+import classes from './StartPage.module.scss'
+import ErrorBox from '../ErrorBox/ErrorBox'
+import { FileUriContext } from '../ContextsProvider/ContextsProvider'
+import PasteFromCbBtn from '../PasteFromCbBtn/PasteFromCbBtn'
 
 const StartPage: FC = () => {
+  const { fileUri, setFileUri } = useContext(FileUriContext)
+
+  const fetchClipboard = async (): Promise<void> => {
+    const pathOrUndefined = await window.api.readClipboard()
+    setFileUri(pathOrUndefined)
+  }
+
+  useEffect(() => {
+    document.addEventListener('paste', fetchClipboard)
+    return (): void => document.removeEventListener('paste', fetchClipboard)
+  }, [])
+
   return (
-    <div>
-      <h1>Document Analizer</h1>
-      <h2>
-        Выберите документ в формате docx, нажав на окно ниже, или перетащив искомый файл в это окно
-      </h2>
+    <div className={classes.container}>
+      <h1 className={classes.title}>Document Analizer</h1>
+      <ErrorBox show={fileUri === undefined}>
+        <p className={classes.text}>
+          The data on the clipboard is not a .docx file or the program was unable to recognize it.
+          Please copy the required .docx file or drag and drop it into the window below.
+        </p>
+      </ErrorBox>
+      <div className={classes.buttons}>
+        <PasteFromCbBtn onClick={fetchClipboard} />
+        <p className={classes.or}>or</p>
+        <PasteFromCbBtn onClick={fetchClipboard} />
+      </div>
     </div>
   )
 }
