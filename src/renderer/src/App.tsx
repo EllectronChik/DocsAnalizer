2ad98129b93f0e4ab97396214ad934f8795a9710
ellectronchik <electronchik04@mail.ru>
2024-09-22 13:35:40 +0300

    added contextsporvider

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,12 @@
 import AppRouter from './components/AppRouter/AppRouter'
+import ContextsProvider from './components/ContextsProvider/ContextsProvider'
 
 function App(): JSX.Element {
-  return <AppRouter />
+  return (
+    <ContextsProvider>
+      <AppRouter />
+    </ContextsProvider>
+  )
 }
 
 export default App
